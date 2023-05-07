fe08e99a7a56f248daf6730618ce1f0524331490
Gergely Pasztor <gpasztor@3-G-11.42heilbronn.de>
2023-05-07 14:03:18 +0200

    Basics set up

---
+++ Makefile
@@ -0,0 +1,33 @@
+NAME	= philosophers
+
+OBJDIR	= obj/
+SRCDIR	= src/
+
+SRC		= philosophers.c
+
+SRCOBJ	= $(patsubst %.c, $(OBJDIR)%.o, $(SRC))
+
+CC		= cc
+RM		= rm -f
+RMDIR	= rmdir
+CFLAGS	= -Wall -Wextra -Werror
+
+$(OBJDIR)%.o: $(SRCDIR)%.c
+	mkdir -p $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(SRCOBJ)
+	$(CC) $(SRCOBJ) $(CFLAGS) -o $(NAME)
+
+clean: 
+	$(RM) $(SRCOBJ)
+	if [ -d "obj" ]; then rmdir obj; fi
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+all: $(NAME)
+
+.PHONY: all clean fclean re libmake
---
+++ includes/philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpasztor <gpasztor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 13:46:25 by gpasztor          #+#    #+#             */
+/*   Updated: 2023/05/07 13:48:44 by gpasztor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <sys/time.h> // time calculation
+# include <pthread.h> // threads
+# include <unistd.h> // usleep
+# include <stdio.h> // printf
+
+#endif
